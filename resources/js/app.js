d1be0fe5354824e0c18eb569abed0dfe8a45843a
Sujan Khadka <khadkaujan774@gmail.com>
2020-12-06 22:06:23 +0545

    configured js and css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
+
+
+// Vue.component('example', require('./components/Example.vue'));
+
+/*const app = new Vue({
+    el: '#app'
+})*/
